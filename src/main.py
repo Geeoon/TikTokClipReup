860aa48179197c3ad712b1c757104848f696ada2
Geeoon Chung <geeoon13@gmail.com>
2022-12-24 00:43:29 -0600

    store secondary videos into array

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+import cv2
+import os
+import numpy as np
+
 if __name__ == "__main__":
-	print("hello world")
+	PRIMARY_PATH = "D:\\Video Projects\\Videos\\Misc\\Primary\\Make Believe.mp4"
+	SECONDARY_DIR = "D:\\Video Projects\\Videos\\Misc"
+	secondary_videos = []
+	for file in os.listdir(SECONDARY_DIR):
+		if file.endswith(".mp4"):
+			secondary_videos.append(os.path.join(SECONDARY_DIR, file))
+	
+	size = (1080, 1920)  # tiktok resolution, although it may be sligithly different
+
+	
